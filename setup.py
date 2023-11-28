dcd4b495b10293cc40d8731269b8c8cf7e311d61
Yuxuan (Tim) Hong <hongyuxuan@gmail.com>
2023-11-28 16:25:01 +1300

    Update setup.py
    updated to python 3.10

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     packages=find_packages(exclude=("tests", )),
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <=3.9.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <=3.10",
     install_requires=[
         "APScheduler==3.5.3",  # Aug 15, 2018
         "click==7.0",  # Sep 26, 2018
